a00246d6451c13e40a1f0ab5599f7bd8f1021c2d
Marcos-Otake <marcos.otake@uni9.edu.br>
2021-03-08 19:21:01 -0300

    foto_alterada

---
+++ lib/screens/detalhes/componentes/detalhes_screen2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:otakehouse/screens/detalhes/componentes/body2.dart';
+
+class DetalhesScreen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Body2(),
+    );
+  }
+}
